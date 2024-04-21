324b73eac588ee11edd6efb18370db139dfcd979
Davint Yuan <davintyuan@gmail.com>
2024-04-21 23:50:20 +0700

    tambah komentar di index

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(isset($_SESSION['login'])){
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
 
     
-
+  //menambah komentar
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
